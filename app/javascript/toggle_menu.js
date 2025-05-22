1b9e2bfcc420ea98b42a66a40ca92650ebc78e72
nezccommand <nezu9000@gmail.com>
2025-05-22 15:53:52 +0900

    add:トップページの中身の作成

--- app/javascript/toggle_menu.js
+++ app/javascript/toggle_menu.js
@@ -3,13 +3,28 @@ document.addEventListener("turbo:load", () => {
   const menu = document.getElementById("dropdown-menu");
 
   if (button && menu) {
+    let menuOpen = false;
+
     button.addEventListener("click", () => {
-      menu.classList.toggle("hidden");
+      menuOpen = !menuOpen;
+      if (menuOpen) {
+        menu.classList.remove("opacity-0", "scale-95", "pointer-events-none");
+        menu.classList.add("opacity-100", "scale-100");
+      } else {
+        menu.classList.add("opacity-0", "scale-95", "pointer-events-none");
+        menu.classList.remove("opacity-100", "scale-100");
+      }
     });
 
     document.addEventListener("click", (event) => {
-      if (!menu.contains(event.target) && !button.contains(event.target)) {
-        menu.classList.add("hidden");
+      if (
+        !menu.contains(event.target) &&
+        !button.contains(event.target) &&
+        menuOpen
+      ) {
+        menu.classList.add("opacity-0", "scale-95", "pointer-events-none");
+        menu.classList.remove("opacity-100", "scale-100");
+        menuOpen = false;
       }
     });
   }

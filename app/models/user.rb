7b2ec80289aca1686cd85522e9ee2d66742e0db4
nezccommand <nezu9000@gmail.com>
2025-05-23 15:21:07 +0900

    add:ログイン/ログアウト機能の動作確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable
 
-  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:encrypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:encrypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:encrypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:encrypted_password] }
   validates :name, presence: true, length: { maximum: 255 }

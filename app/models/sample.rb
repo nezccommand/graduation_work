df1b3f9bdd46be492dc8dc472892863051bf626a
nezccommand <nezu9000@gmail.com>
2025-06-05 19:57:39 +0900

    fix:lintチェック

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,12 +1,12 @@
 class Sample < ApplicationRecord
   has_many :sample_tags, dependent: :destroy
   has_many :tags, through: :sample_tags
-  
+
   def self.ransackable_attributes(auth_object = nil)
-    ["title", "description"]
+    [ "title", "description" ]
   end
 
   def self.ransackable_associations(auth_object = nil)
-    ["tags"]
+    [ "tags" ]
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,10 +3,10 @@ class Tag < ApplicationRecord
   has_many :samples, through: :sample_tags
 
   def self.ransackable_associations(auth_object = nil)
-    ["sample_tags", "samples"]
+    [ "sample_tags", "samples" ]
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["id", "name"]
+    [ "id", "name" ]
   end
 end

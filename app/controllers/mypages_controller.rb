75d3e65b2c9918b3a2e6086dfca286ffffd711da
nezccommand <nezu9000@gmail.com>
2025-06-08 23:45:51 +0900

    add:コントローラーを少し変更

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,7 +2,6 @@ class MypagesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    # 仮置きデータ例（テーブル未作成なのでコメント）
     # @quiz_histories = current_user.quiz_histories
     # @favorite_samples = current_user.favorite_samples
   end

b9bd5bc7a0eca00ee6a0257f0a81b37a1b2bb497
nezccommand <nezu9000@gmail.com>
2025-06-10 17:12:55 +0900

    add:模擬メール送信機能、テンプレート、及びここまでのデータベースのバックアップを作成

--- app/controllers/simulation_emails_controller.rb
+++ app/controllers/simulation_emails_controller.rb
@@ -2,16 +2,13 @@ class SimulationEmailsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    # 説明画面（メール送信ボタンなど）
   end
 
   def create
-    # ランダムなテンプレートでメール送信
     PhishingMailer.send_random_email(current_user).deliver_now
-    redirect_to complete_training_email_path
+    redirect_to complete_simulation_email_path
   end
 
   def complete
-    # 送信完了画面
   end
 end

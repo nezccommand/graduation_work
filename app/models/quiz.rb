3bd25f2faa9b3872f172537ce873f52a3e07c04a
nezccommand <nezu9000@gmail.com>
2025-07-14 14:49:27 +0900

    add: クイズの難易度カラムを追加

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,6 +2,8 @@ class Quiz < ApplicationRecord
   has_many :choices, dependent: :destroy
   accepts_nested_attributes_for :choices, allow_destroy: true
 
+  enum difficulty: { easy: "easy", hard: "hard" }
+
   def self.calculate_results(quiz_ids, answer_ids)
     quizzes = find(quiz_ids)
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_24_072448) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_14_054314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_24_072448) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre", null: false
+    t.string "difficulty", default: "easy", null: false
   end
 
   create_table "sample_tags", force: :cascade do |t|

3a130eb263a2be760ad34b55dd9c181d9a90f70e
nezccommand <nezu9000@gmail.com>
2025-07-25 14:31:48 +0900

    fix: マイページからクイズがスタートできないのを修正

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,7 @@
 class Badge < ApplicationRecord
+  has_many :user_badges, dependent: :destroy
+  has_many :users, through: :user_badges
+
+  validates :difficulty, presence: true
+  validates :genre, presence: true
 end
